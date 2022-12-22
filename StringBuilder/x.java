43c12a5006946a4b0f1b2f24f21877ce5bf58f2a
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2022-12-22 19:34:27 +0530

    Add files via upload

---
+++ StringBuilder/x.java
@@ -0,0 +1,14 @@
+package StringBuilder;
+
+public class x {
+    public static void main(String args[]){
+        Integer name=new Integer(68);
+        String a="aman";
+        System.out.println(a.charAt(1));
+    }
+
+static int[] arr(){
+    return new int[]{2,3,4,5,6};
+
+}
+}
