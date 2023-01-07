79ee0a6b391aa1157ccfe9e037764132c6f30d4a
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2023-01-08 00:13:09 +0530

    Add files via upload

---
+++ Strings_and_StringBuilder/LeetCode_709.java
@@ -0,0 +1,11 @@
+package Strings_and_StringBuilder;
+
+public class LeetCode_709 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
