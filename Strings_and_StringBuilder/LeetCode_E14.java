13b5efb6c3ba0e2837bdb661ae3592fd6057d03a
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2023-01-17 23:00:41 +0530

    Add files via upload

---
+++ Strings_and_StringBuilder/LeetCode_E14.java
@@ -0,0 +1,18 @@
+package Strings_and_StringBuilder;
+
+public class LeetCode_E14 {
+    public static void main(String[] args) {
+       String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int index=1;index<strs.length;index++){
+            while(strs[index].indexOf(prefix) != 0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
+
