3902a3e6a22c986172bf3ff0fbeb37f17bd77276
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2023-01-20 22:07:22 +0530

    Add files via upload

---
+++ MathsForDSA/LeetCode_2413.java
@@ -0,0 +1,12 @@
+package MathsForDSA;
+
+public class LeetCode_2413 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(smallestEvenMultiple(n));
+    }
+
+    public static int smallestEvenMultiple(int n) {
+        return n % 2 == 0 ? n : n * 2;
+    }
+}
