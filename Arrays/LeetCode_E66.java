d34d8fe06d22f0cdf6fbbfa67603e24d56f76b36
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2023-01-29 21:40:16 +0530

    Add files via upload

---
+++ Arrays/LeetCode_E66.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class LeetCode_E66 {
+    public static void main(String[] args){
+        int[] digits={1,2,3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
