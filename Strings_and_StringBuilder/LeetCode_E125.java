a16f2fb5f4da00ebfa4c614956ee7077f2919a70
Aman Kumar Singh <119577598+aman-singhsm01@users.noreply.github.com>
2023-02-01 19:20:10 +0530

    Add files via upload

---
+++ Strings_and_StringBuilder/LeetCode_E125.java
@@ -0,0 +1,22 @@
+package Strings_and_StringBuilder;
+
+public class LeetCode_E125 {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int i = 0;
+        int j = s.length() - 1;
+        while(i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
